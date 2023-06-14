0b74e9bdba0ac2842a956e5538746e17dd70f5e8
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-14 16:53:39 +0800

    chore: add support for new data file types in package
    - Add new dependencies to `package.json`
    - Add new data files: `data.json5`, `data.toml`, `data.yaml`
    - Modify `index.js` to import and use the new data files
    - Modify `webpack.config.js` to handle the new data file types
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
   entry: './src/index.js',
@@ -22,13 +25,34 @@ module.exports = {
         type: 'asset/resource',
       },
       {
-          test: /\.(csv|tsv)$/i,
-          use: ['csv-loader'],
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
       },
       {
-          test: /\.xml$/i,
-          use: ['xml-loader'],
-      }
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
     ],
   },
   mode: 'development',
